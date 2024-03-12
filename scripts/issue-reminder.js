8dacf397df0cbafe2afc1f0c69d17537ec328f31
YuyaYoshino <150419511+YuyaYoshino@users.noreply.github.com>
2024-03-12 13:40:01 +0900

    Update issue-reminder.js

--- scripts/issue-reminder.js
+++ scripts/issue-reminder.js
@@ -2,7 +2,7 @@ const { Octokit } = require("@octokit/rest");
 const fetch = require('node-fetch');
 
 const octokit = new Octokit({
-  auth: process.env.GITHUB_TOKEN,
+  auth: process.env.MY_PERSONAL_TOKEN,
   request: {
     fetch: fetch
   }
